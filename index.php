8f42a4d57b1f550a85cfb25638ea2bec3dc9f97c
Ziv0921 <zivwjk714@gmail.com>
2025-09-30 13:56:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">迎新茶會</h5>
-                        <p class="card-text">參加迎新茶會，認識新朋友。</p>
+                        <p class="card-text">迎新茶會是專為新生設計的交流活動，讓新同學能夠認識師長與學長姐，了解資管系的學習環境與資源。活動中有輕鬆的茶點、趣味破冰遊戲，以及學長姐經驗分享，幫助新生快速融入大學生活。</p>
                         <a href="status.php" class="btn btn-primary">報名</a>
                     </div>
                 </div>
@@ -16,7 +16,7 @@
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">資管一日營</h5>
-                        <p class="card-text">體驗資管一日營，學習新知識。</p>
+                        <p class="card-text">資管一日營邀請大一新生透過一整天的活動更大學資管系的課程與生活。活動內容包含常用網站介紹、校園導覽與學長姐座談、闖關遊戲，讓參加者為未來四年作好準備。。</p>
                         <a href="conference.php" class="btn btn-primary">報名</a>
                     </div>
                 </div>
